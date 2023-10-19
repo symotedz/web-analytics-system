3691cb4e647348eeeda3c447b9797e81a775480a
Simon Kamau <simonkamau690@gmail.com>
2023-10-19 15:31:39 +0300

    update result view
    update result view

--- web_student/ResultView.py
+++ web_student/ResultView.py
@@ -7,6 +7,8 @@ from web_super_admin.forms import *
 
 def exam_result_view(request, pk):
     exam_result = get_object_or_404(ExamResult, pk=pk)
-    
-    
+    context = {
+        'exam_result': exam_result,
+    }
+    return render(request, 'index.html', context)
 
