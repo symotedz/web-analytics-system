b4adce9fc90116420d2183ec1bbd8c5568bc24fa
Simon Kamau <simonkamau690@gmail.com>
2023-10-19 15:35:26 +0300

    serializers.
    serializers.

---
+++ api_accountant/serializers.py
@@ -0,0 +1,7 @@
+from web_super_admin import models 
+from api_super_admin.models import *
+
+from rest_framework import serializer
+import datetime as datetime
+
+from django.shortcuts import render, redirect, get_object_or_404
