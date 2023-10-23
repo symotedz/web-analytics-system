1dcb12da209f58d330387b77f17ef9e0b4e9645e
Simon Kamau <simonkamau690@gmail.com>
2023-10-23 15:01:22 +0300

    implemented middleware
    implemented middleware

--- middleware/authmiddleware.py
+++ middleware/authmiddleware.py
@@ -1,2 +1,12 @@
+from typing import Any
 from django.contrib.auth import authenticate
 
+
+class AnanlyticsAuthenticationMiddleware:
+    def __init__(self, get_response) -> None:
+        self.get_response = get_response
+        
+    def __call__(self, request) -> Any:
+        response = self.get_response(request)
+        
+        return response
