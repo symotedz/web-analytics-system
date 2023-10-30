037996ead0dbaf5cce48b66a089ba311517f1372
Simon Kamau <simonkamau690@gmail.com>
2023-10-30 16:02:26 +0300

    serializers update
    serializers update

--- api_admin/serializers.py
+++ api_admin/serializers.py
@@ -17,4 +17,62 @@ class StaffSerializer(serializers.ModelSerializer):
     class Meta:
         model = Staff 
         fields = '__all__'
+        
+class School_blocksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = School_blocks
+        fields = '__all__'
+        
+class class_blockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = class_block
+        fields = '__all__'
+        
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student 
+        fields = '__all__'
+        
+
+
+class class_createSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = class_create
+        fields = '__all__'
+        
+class FeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = class_create
+        fields = '__all__'
+        
+class LibraryCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LibraryCategory
+        fields = '__all__'
+        
+class LibraryItemCopySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LibraryItemCopy
+        fields = '__all__'
+        
+class LibraryReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LibraryReservation
+        fields = '__all__'
+        
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LibraryReservation
+        fields = 'LibraryReservation'
+        
+class NoticeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LibraryReservation
+        fields = '__all__'
+        
+        
+        
+        
+        
+        
 
