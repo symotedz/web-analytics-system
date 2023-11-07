5b1304712e567846b0f5e736acd71299462eda5c
Simon Kamau <simonkamau690@gmail.com>
2023-11-06 17:45:32 -0800

    Update views.py
    Update views.py

--- api_teacher/views.py
+++ api_teacher/views.py
@@ -1,6 +1,68 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello,World")
+from .serializers import *
+from web_super_admin.models import *
+
+
+class TeacherListCreateView(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+
+class class_blockListCreateView(generics.ListCreateAPIView):
+    queryset = class_block.objects.all()
+    serializer_class = class_blockSerializer
+
+class StudentListCreateView(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class EventListCreateView(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    
+class NoticeListCreateView(generics.ListCreateAPIView):
+    queryset = Notice.objects.all()
+    serializer_class = NoticeSerializer
+    
+class SubjectListCreateView(generics.ListCreateAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+    
+class ExamListCreateView(generics.ListCreateAPIView):
+    queryset = Exam.objects.all()
+    serializer_class = ExamSerializer
+    
+class ExamResultListCreateView(generics.ListCreateAPIView):
+    queryset = ExamResult.objects.all()
+    serializer_class = ExamResultSerializer
+    
+class AttendanceListCreateView(generics.ListCreateAPIView):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+    
+class AssignmentListCreateView(generics.ListCreateAPIView):
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentSerializer
+    
+class GradeListCreateView(generics.ListCreateAPIView):
+    queryset = Grade.objects.all()
+    serializer_class = GradeSerializer
+
+class OpportunitiesListCreateView(generics.ListCreateAPIView):
+    queryset = Opportunities.objects.all()
+    serializer_class = OpportunitiesSerializer
+    
+class ELearningListCreateView(generics.ListCreateAPIView):
+    queryset = ELearning.objects.all()
+    serializer_class = ELearningSerializer
+    
+class MessageListCreateView(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    
+class TimeTableListCreateView(generics.ListCreateAPIView):
+    queryset = TimeTable.objects.all()
+    serializer_class = TimeTableSerializer
