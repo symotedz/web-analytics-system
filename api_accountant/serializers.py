3cf36b4da9a0d2461d8133dad67a7ca369b65752
Simon Kamau <simonkamau690@gmail.com>
2023-11-06 17:26:33 -0800

    implemented accountant api
    implemented accountant api

--- api_accountant/serializers.py
+++ api_accountant/serializers.py
@@ -2,6 +2,33 @@ from rest_framework import serializer
 import datetime as datetime
 
 from django.shortcuts import render, redirect, get_object_or_404
+from rest_framework import serializers
 
-from web_super_admin import models 
+from web_super_admin.models import *
 from api_super_admin.models import *
+
+class StaffSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Staff 
+        fields = "__all__"
+
+class FeeSerializer(serializer.ModelSerializer):
+    class Meta:
+        model = Fee 
+        fields = "__all__"
+
+class NoticeSerializer(serializer.ModelSerializer):
+    class Meta:
+        model  = Notice 
+        fields = "__all__"
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event 
+        fields = "__all__"
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message 
+        fields = "__all__"
+
--- api_accountant/views.py
+++ api_accountant/views.py
@@ -1,6 +1,38 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from rest_framework import generics
+
+
+from web_super_admin.models import *
+from . serializers import *
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World")
+
+class StaffListCreateView(generics.ListCreateAPIView):
+    staffs = Staff.objects.all()
+    serializer_class = StaffSerializer
+
+class FeeListCreateView(generics.ListCreateAPIView):
+    fee = Fee.objects.all()
+    serializer_class = FeeSerializer
+
+class NoticeListCreateView(generics.ListCreateAPIView):
+    notice = Notice.objects.all()
+    serializer_class = NoticeSerializer
+
+class EventListCreateView(generics.ListCreateAPIView):
+    Event = Event.objects.all()
+    serializer_class = EventSerializer
+
+class MessageListCreateView(generics.ListCreateAPIView):
+    messages = Message.objects.all()
+    serializer_class = MessageSerializer
+
+
+
+
