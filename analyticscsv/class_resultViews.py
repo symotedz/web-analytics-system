14dbde615016a4ec76be0b37c91d697ccd7dd13a
Simon Kamau <simonkamau690@gmail.com>
2023-11-21 16:42:41 +0300

    class result csv implementation

--- analyticscsv/class_resultViews.py
+++ analyticscsv/class_resultViews.py
@@ -1,1 +1,13 @@
 from datetime import datetime
+import csv
+
+from django.http import HttpResponse
+
+from .models import *
+
+def export_csv_view(request):
+    response = HttpResponse(content_type = "text/csv")
+    response['content-disposition'] = 'attachment: file_name="data.csv"'
+    writer = csv.writer(["FirstName", "LastName", "Proffesional"])
+    writer = csv.writer(["simon", "kamau", "Software Engineer"])
+    return response
