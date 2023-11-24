405189a59fd8b3c8005218dfd2cb9ce57de2760d
Simon Kamau <simonkamau690@gmail.com>
2023-11-24 16:09:02 +0300

    set up accountant routes

--- web_accountant/urls.py
+++ web_accountant/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from . import views 
+from . import views, fee_views
 
 app_name  = 'web_accountant'
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+
+        # urls for fees view
+    path('fee_create/', fee_views.fee_create, name='fee_create'),
+    path('fees_detail/', fee_views.fees_detail, name='fees_detail'),
+    path('fee_detail/<int:pk>/', fee_views.fee_detail, name='fee_detail'),
+    path('fee_update/<int:pk>/', fee_views.fee_update, name='fee_update'),
+    path('fee_delete/<int:pk>/', fee_views.fee_delete, name='fee_delete'),
+    path('fees_delete/', fee_views.fees_delete, name='fees_delete'),
+    
 ]
