b22574f8cd3490d19b1d974e4341a9f16def8848
Simon Kamau <simonkamau690@gmail.com>
2023-11-24 17:01:10 +0300

    financial data analysis set up

---
+++ dataAnalysis/financial/consolidated.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import URLResolver
---
+++ dataAnalysis/financial/monthly.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+
+import numpy as np
+import pandas as pd 
+
+def financial_analysis(request):
+    pass
