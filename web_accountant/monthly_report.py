b92fd8ca3f91a99c1abbd2ae150e6f6f737ea296
Simon Kamau <simonkamau690@gmail.com>
2023-11-27 17:42:36 +0300

    monthly report

--- web_accountant/monthly_report.py
+++ web_accountant/monthly_report.py
@@ -1,5 +1,12 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.urls import url
+
 from web_super_admin.models import *
 from web_super_admin.forms import *
 
 from .models import *
 from .forms import *
+
+def monthlyReport(request):
+    pass
