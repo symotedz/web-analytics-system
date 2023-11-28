12467cd58f840b44ea82a67b79a671be99895367
Simon Kamau <simonkamau690@gmail.com>
2023-11-28 16:56:37 +0300

    urls for teacher

--- api_librarian/urls.py
+++ api_librarian/urls.py
@@ -13,5 +13,42 @@ from . views import OpportunitiesListCreateView, OpportunitiesRetrieveUpdateDest
 app_name = 'api_librarian'
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+
+     # urls for library category
+    path('librarycategories/', LibraryCategoryListCreateView.as_view(), name='LibraryCategoryListCreateView'),
+    path('librarycategory/<int:pk>/', LibraryCategoryRetrieveUpdateDestroyView.as_view(), name="LibraryCategoryRetrieveUpdateDestroyView"),
+
+    # urls for library item
+    path('libraryitems/', LibraryItemListCreateView.as_view(), name='LibraryItemListCreateView'),
+    path('libraryitem/<int:pk>/', LibraryItemRetrieveUpdateDestroyView.as_view(), name="LibraryItemRetrieveUpdateDestroyView"),
+
+    # urls for library item copy
+    path('libraryitemcopies/', LibraryItemCopyListCreateView.as_view(), name='LibraryItemCopyListCreateView'),
+    path('libraryitemcopy/', LibraryItemCopyRetrieveUpdateDestroyView.as_view(), name='LibraryItemCopyRetrieveUpdateDestroyView'),
+
+    # urls for library researvation
+    path('libraryreservations/', LibraryReservationListCreateView.as_view(), name='LibraryReservationListCreateView'),
+    path('libraryresearvation/<int:pk>/', LibraryReservationRetrieveUpdateDestroy.as_view(), name='LibraryReservationRetrieveUpdateDestroy'),
+
+    # urls for staffs
+    path('staffs/', StaffListCreateView.as_view(), name='StaffListCreateView'),
+    path('staff/<int:pk>/', StaffRetrieveUpdateDestroyView.as_view(), name='StaffRetrieveUpdateDestroyView'),
+
+    #urls for events
+    path('events/', EventListCreateView.as_view(), name='EventListCreateView'),
+    path('event/<int:pk>/', EventRetrieveUpdateDestroyView.as_view(), name='EventRetrieveUpdateDestroyView'),
+
+    # urls for notices
+    path('notice/', NoticeListCreateView.as_view(), name='NoticeListCreateView'),
+    path('notice/<int:pk>/', NoticeRetrieveUpdateDestroyView.as_view(), name='NoticeRetrieveUpdateDestroyView'),
+
+    # urls for students
+    path('students/', StudentListCreateView.as_view(), name='StudentListCreateView'),
+    path('student/<int:pk>/', StudentRetrieveUpdateDestroyView.as_view(), name='StudentRetrieveUpdateDestroyView'),
+
+    # urls for opportunities
+    path('opportunities/', OpportunitiesListCreateView.as_view(), name='OpportunitiesListCreateView'),
+    path('opportunity/<int:pk>/', OpportunitiesRetrieveUpdateDestroyView.as_view(), name='OpportunitiesRetrieveUpdateDestroyView'),
+
 ]
