4efe1ccabd0d954726bdc59e2fb4e0c6765d9c88
Simon Kamau <simonkamau690@gmail.com>
2023-11-28 16:36:44 +0300

    commit

--- api_librarian/urls.py
+++ api_librarian/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
 from . import views
+from . views import LibraryCategoryListCreateView, LibraryCategoryRetrieveUpdateDestroyView
+from . views import LibraryItemListCreateView, LibraryItemRetrieveUpdateDestroyView
+from . views import LibraryReservationListCreateView, LibraryReservationRetrieveUpdateDestroy
+from . views import LibraryItemCopyListCreateView, LibraryItemCopyRetrieveUpdateDestroyView
+from . views import StaffListCreateView, StaffRetrieveUpdateDestroyView
+from . views import NoticeListCreateView, NoticeRetrieveUpdateDestroyView
+from . views import EventListCreateView, EventRetrieveUpdateDestroyView
+from . views import StudentListCreateView, StudentRetrieveUpdateDestroyView
+from . views import OpportunitiesListCreateView, OpportunitiesRetrieveUpdateDestroyView
 
 app_name = 'api_librarian'
 
