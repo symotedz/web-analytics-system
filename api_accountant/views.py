9cda04cad95e2be751727e4daa6967f241920582
Simon Kamau <simonkamau690@gmail.com>
2023-11-28 10:31:59 +0300

    accountant api complete implementation

--- api_accountant/views.py
+++ api_accountant/views.py
@@ -17,11 +17,22 @@ class StaffListCreateView(generics.ListCreateAPIView):
     staffs = Staff.objects.all()
     serializer_class = StaffSerializer
 
+class StaffRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    staffs = Staff.objects.all()
+    serializer_class = StaffSerializer
+
 class FeeListCreateView(generics.ListCreateAPIView):
     fee = Fee.objects.all()
     serializer_class = FeeSerializer
 
+class FeeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    fee = Fee.objects.all()
+    serializer_class = FeeSerializer
 class NoticeListCreateView(generics.ListCreateAPIView):
+    notice = Notice.objects.all()
+    serializer_class = NoticeSerializer
+
+class NoticeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     notice = Notice.objects.all()
     serializer_class = NoticeSerializer
 
@@ -29,7 +40,15 @@ class EventListCreateView(generics.ListCreateAPIView):
     Event = Event.objects.all()
     serializer_class = EventSerializer
 
+class EventRetrieveUpdateDestroyUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    event = Event.objects.all()
+    serializer_class = EventSerializer
+
 class MessageListCreateView(generics.ListCreateAPIView):
+    messages = Message.objects.all()
+    serializer_class = MessageSerializer
+
+class MessageRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     messages = Message.objects.all()
     serializer_class = MessageSerializer
 
