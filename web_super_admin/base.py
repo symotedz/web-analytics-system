d8d63db9cda2f53799a5a722daff12f2f96fe7f0
Simon Kamau <simonkamau690@gmail.com>
2023-12-05 15:16:37 +0300

    modificcations

---
+++ web_super_admin/base.py
@@ -0,0 +1,21 @@
+from datetime import datetime 
+from django.shortcuts import render, redirect, get_object_or_404
+from Analytics.systemSettings import *
+
+
+system_name = SYSTEM_NAME
+copyright = COPYRIGHT
+logo = LOGO
+ipadress = IP_ADRESS
+
+def base(request):
+    context = {
+        'system_name' : system_name,
+        'copyright' : copyright,
+        'logo' : logo,
+        'ipadress' : ipadress
+    }
+    return render(request, "Dashboard/base.html", context)
+
+print(SYSTEM_NAME)
+
