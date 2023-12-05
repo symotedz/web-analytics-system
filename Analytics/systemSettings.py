5e94c3bcca704da3d0d0c9483715721218d081be
Simon Kamau <simonkamau690@gmail.com>
2023-12-05 14:19:31 +0300

    systemSettings implementation

---
+++ Analytics/systemSettings.py
@@ -0,0 +1,8 @@
+from datetime import datetime 
+from django.http import HttpResponse
+from django.utils import duration
+
+SYSTEM_NAME = "Analytics System"
+COPYRIGHT =  "© copyright  2023" + SYSTEM_NAME
+LOGO  = "path/to/logo"
+IP_ADRESS = "192.168.6.45"
---
+++ web_super_admin/system.py
@@ -0,0 +1,4 @@
+from datetime import datetime 
+from django.db import models 
+from django.http import HttpResponse
+
