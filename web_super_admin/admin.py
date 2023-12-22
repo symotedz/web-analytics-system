e8d7dbd28dbd75b05105f51891a1e54ff61f42c5
Simon Kamau <simonkamau690@gmail.com>
2023-12-22 14:04:51 +0300

    enhanced admin models

--- web_super_admin/admin.py
+++ web_super_admin/admin.py
@@ -5,13 +5,23 @@ from .models import Super_Admin_User, School
 
 @admin.register(Super_Admin_User)
 class SuperAdminUserAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'Registration_date', 'status', 'end_date')
+
+    def end_date(self, obj):
+        return obj.End_Date if obj.End_Date else ''
+    
+    end_date.short_description = 'End Date'
 
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
-    list_display = ('name', 'super_admin_user_name')
+    list_display = ('name', 'super_admin_user_name', 'Registration_date', 'status', 'end_date')
 
     def super_admin_user_name(self, obj):
         return obj.user.name if obj.user else ''
 
     super_admin_user_name.short_description = 'Super Admin User Name'
+
+    def end_date(self, obj):
+        return obj.End_Date if obj.End_Date else ''
+    
+    end_date.short_description = 'End Date'
--- web_super_admin/models.py
+++ web_super_admin/models.py
@@ -14,6 +14,8 @@ class Super_Admin_User(models.Model):
     Registration_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     status = models.BooleanField(default=True, blank=True, null=True)
     End_Date = models.DateField(blank=True, null=True)
+    contact = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
 
 class School(models.Model):
     name = models.CharField(max_length=255)
@@ -21,6 +23,10 @@ class School(models.Model):
     Registration_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     status = models.BooleanField(default=True, null=True, blank=True)
     End_Date = models.DateField(blank=True, null=True)
+    County = models.CharField(max_length=255,blank=True, null=True)
+    location = models.CharField(max_length=255,blank=True, null=True)
+    contact = models.CharField(max_length=255,blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
     
 
 
